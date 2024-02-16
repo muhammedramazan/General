e088ed3a0254d6c53b1ad057fdac16c1b87a61d0
Ramazan <arge5@avciss.com>
2024-02-16 17:46:54 +0300

    ...

---
+++ C++ Primer/Chapter3/3_21.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void print_element_string(vector<string> v) {
+  cout << "Size : " << v.size() << endl;
+  cout << "Element List ::";
+  cout << endl;
+  for (auto i = v.begin(); i != v.end(); i++) cout << " " << *i;
+  cout << endl;
+}
+
+void print_element_int(vector<int> v) {
+  cout << "Size : " << v.size() << endl;
+  cout << "Element List ::";
+  for (auto i = v.begin(); i != v.end(); i++) cout << " " << *i;
+  cout << endl;
+}
+int main(int argc, char const *argv[]) {
+  vector<int> v1;
+  vector<int> v2(10);
+  vector<int> v3(10, 42);
+  vector<int> v4{10};
+  vector<int> v5{10, 42};
+  vector<string> v6{10};
+  vector<string> v7{10, "hi"};
+  print_element_int(v1);
+  print_element_int(v2);
+  print_element_int(v3);
+  print_element_int(v4);
+  print_element_int(v5);
+  print_element_string(v6);
+  print_element_string(v7);
+  return 0;
+}
